f821c3462b1c37cea54503af59b82a77a562ca37
范保莹 <fanbaoying@agree.com.cn>
2017-10-23 14:34:43 +0800

    时间选择器

--- DatePicker/ViewController.m
+++ DatePicker/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+
+@interface ViewController ()<UITextFieldDelegate>
+
+@property (nonatomic, strong) UIDatePicker *datePicker;
+
+@property(strong,nonatomic)UITextField *timeTextField;
 
 @end
 
@@ -16,7 +23,78 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    
+    self.timeTextField = [[UITextField alloc]initWithFrame:CGRectMake(10, 200, SCREEN_WIDTH-20, 50)];
+    self.timeTextField.textColor = [UIColor blueColor];
+    self.timeTextField.layer.borderWidth = 1.0;
+    self.timeTextField.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    self.timeTextField.placeholder = @"请选择时间";
+    self.timeTextField.textAlignment = NSTextAlignmentCenter;
+    self.timeTextField.delegate = self;
+    [self.view addSubview:_timeTextField];
+    
+    
+    [self setupDateKeyPan];
+}
+
+//隐藏时间表盘
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+    [self.view endEditing:YES];
+    
+}
+
+- (void)setupDateKeyPan {
+    
+    UIDatePicker *datePicker = [[UIDatePicker alloc] init];
+    
+    //设置地区: zh-中国
+    datePicker.locale = [NSLocale localeWithLocaleIdentifier:@"zh"];
+    
+    //设置日期模式(Displays month, day, and year depending on the locale setting)
+    datePicker.datePickerMode = UIDatePickerModeCountDownTimer;
+    // 设置当前显示时间
+    [datePicker setDate:[NSDate date] animated:YES];
+    // 设置显示最大时间（此处为当前时间）
+    [datePicker setMaximumDate:[NSDate date]];
+    
+    //设置时间格式
+    
+    //监听DataPicker的滚动
+    [datePicker addTarget:self action:@selector(dateChange:) forControlEvents:UIControlEventValueChanged];
+    
+    self.datePicker = datePicker;
+    
+    //设置时间输入框的键盘框样式为时间选择器
+    self.timeTextField.inputView = datePicker;
+}
+
+//禁止用户输入文字
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+  return NO;
+}
+
+//became first responder(已经成为第一响应者的时候调用), 目的是为了弹出键盘, TF默认显示当前日期
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    
+    [self dateChange:_datePicker];
+    
+}
+
+- (void)dateChange:(UIDatePicker *)datePicker {
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    
+    //设置时间格式
+    formatter.dateFormat = @"yyyy年 MM月 dd日";
+    NSString *dateStr = [formatter  stringFromDate:datePicker.date];
+    
+    self.timeTextField.text = dateStr;
 }
 
 
